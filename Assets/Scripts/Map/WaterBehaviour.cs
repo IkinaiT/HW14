9e388ca9e7e3204ed8abac2e746c37ce72f39296
IkinaiT <ikinai@yandex.ru>
2025-01-10 00:01:11 +0300

    Add level 3

---
+++ Assets/Scripts/Map/WaterBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBehaviour : MonoBehaviour
+{
+    [SerializeField, Range(0, 30)]
+    private float _speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(other.GetComponent<PlayerStatus>().GetSize() == PlayerSize.Big)
+                other.GetComponent<Rigidbody>().AddForce(Vector3.up * _speed, ForceMode.Force);
+        }
+    }
+}
